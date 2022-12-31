57c092ccaff027850e1202888390ea5b5f611bcb
jenboc <47900748+jenboc@users.noreply.github.com>
2022-12-31 15:37:49 +0000

    MidiManager.ReadFile

---
+++ Apollo.MIDI/MidiManager.cs
@@ -0,0 +1,85 @@
+﻿using NAudio.Midi;
+namespace Apollo.MIDI;
+
+public static class MidiManager
+{
+    public static List<string> ReadFile(string path)
+    {
+        if (GetPathType(path) != 'f')
+            throw new FileNotFoundException($"{path} is not a valid file path");
+        
+        var file = new MidiFile(path, false);
+        var data = new List<string>();
+
+        data.Add(file.DeltaTicksPerQuarterNote.ToString());
+
+        for (var trackNum = 0; trackNum < file.Tracks; trackNum++)
+        {
+            foreach (var e in file.Events[trackNum])
+            {
+                if (e.GetType() == typeof(NoteOnEvent))
+                {
+                    try
+                    {
+                        var onEvent = (NoteOnEvent)e;
+                        var offEvent = onEvent.OffEvent;
+
+                        var noteNumber = onEvent.NoteNumber;
+                        var onTime = onEvent.AbsoluteTime;
+                        var offTime = offEvent.AbsoluteTime;
+                        var noteLength = onEvent.NoteLength;
+                        var velocity = onEvent.Velocity;
+
+                        data.Add($"P{noteNumber}");
+                        data.Add(onTime.ToString());
+                        data.Add(offTime.ToString());
+                        data.Add(noteLength.ToString());
+                        data.Add(velocity.ToString());
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+                else if (e.GetType() == typeof(TempoEvent))
+                {
+                    var tempoEvent = (TempoEvent)e;
+                    data.Add($"T{tempoEvent.MicrosecondsPerQuarterNote}");
+                    data.Add(tempoEvent.AbsoluteTime.ToString());
+                }
+            }
+        }
+
+        return data;
+    }
+
+    public static void ReadDir(string path)
+    {
+        if (GetPathType(path) != 'd')
+            throw new DirectoryNotFoundException($"{path} is not a valid directory");
+    }
+
+    public static void WriteFile(List<string> data)
+    {
+        
+    }
+    public static void WriteFile(string data)
+    {
+        WriteFile(data.Split('\n').ToList());
+    }
+    
+    /// <summary>
+    /// Checks whether a path is valid 
+    /// </summary>
+    /// <param name="path">The path to validate</param>
+    /// <returns>A character flag (d => directory, f => file, n => invalid path)</returns>
+    private static char GetPathType(string path)
+    {
+        var attributes = File.GetAttributes(path);
+
+        if (attributes == FileAttributes.Directory && Directory.Exists(path))
+            return 'd';
+
+        return File.Exists(path) ? 'f' : 'n';
+    }
+}
--- TestingConsoleApp/Program.cs
+++ TestingConsoleApp/Program.cs
@@ -1,26 +1,96 @@
-﻿using Apollo.MatrixMaths;
-using Apollo.NeuralNet;
-using System.Text.Json;
+﻿using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using NAudio.Midi;
 
-var vocabSize = 3;
-var recurrenceLength = 100;
-var learningRate = 10e-5f;
+const string PATH = @"bach_846.mid";
 
-var initialInput = Matrix.Random(vocabSize, 1);
-
-var r = new Rnn(vocabSize, recurrenceLength, learningRate);
-
-var outputs = r.Forward(initialInput);
-
-foreach (var o in outputs) 
+static List<string> ReadFile()
 {
-    for (var i = 0; i < o.Rows; i++)
+    var file = new MidiFile(PATH, false);
+    var data = new List<string>();
+
+    data.Add(file.DeltaTicksPerQuarterNote.ToString());
+
+    for (var trackNum = 0; trackNum < file.Tracks; trackNum++)
     {
-        for (var j = 0; j < o.Columns; j++)
+        foreach (var e in file.Events[trackNum])
         {
-            Console.Write($"{o.Contents[i,j]}\t");
+            if (e.GetType() == typeof(NoteOnEvent))
+            {
+                try
+                {
+                    var onEvent = (NoteOnEvent)e;
+                    var offEvent = onEvent.OffEvent;
+
+                    var noteNumber = onEvent.NoteNumber;
+                    var onTime = onEvent.AbsoluteTime;
+                    var offTime = offEvent.AbsoluteTime;
+                    var noteLength = onEvent.NoteLength;
+                    var velocity = onEvent.Velocity;
+
+                    data.Add($"P{noteNumber}");
+                    data.Add(onTime.ToString());
+                    data.Add(offTime.ToString());
+                    data.Add(noteLength.ToString());
+                    data.Add(velocity.ToString());
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            else if (e.GetType() == typeof(TempoEvent))
+            {
+                var tempoEvent = (TempoEvent)e;
+                data.Add($"T{tempoEvent.MicrosecondsPerQuarterNote}");
+                data.Add(tempoEvent.AbsoluteTime.ToString());
+            }
         }
-        Console.WriteLine();
     }
-    Console.WriteLine("\n\n\n\n");
+
+    return data;
 }
+
+static void WriteFile(List<string> data)
+{
+    var dTicks = int.Parse(data[0]);
+    var collection = new MidiEventCollection(0, dTicks);
+
+    var eventStart = 1;
+    for (var i = 1; i < data.Count; i++)
+    {
+        var line = data[i];
+        if (char.IsLetter(line[0]) && eventStart != i) // Everything before i is apart of a singular event
+        {
+            // Tempo event uses 2 lines, note event uses 4
+            if (data[eventStart][0] == 'T')
+            {
+                var microsecondsPerQuarterNote = int.Parse(data[eventStart].Substring(1));
+                var absoluteTime = long.Parse(data[eventStart + 1]);
+                collection.AddEvent(new TempoEvent(microsecondsPerQuarterNote, absoluteTime), 1);
+            }
+            else if (data[eventStart][0] == 'P')
+            {
+                var noteNumber = int.Parse(data[eventStart].Substring(1));
+                var onEventTime = long.Parse(data[eventStart + 1]);
+                var offEventTime = long.Parse(data[eventStart + 2]);
+                var duration = int.Parse(data[eventStart + 3]);
+                var velocity = int.Parse(data[eventStart + 4]);
+                
+                collection.AddEvent(new NoteOnEvent(onEventTime, 1, noteNumber, velocity, duration), 1);
+                collection.AddEvent(new NoteEvent(offEventTime, 1, MidiCommandCode.NoteOff, noteNumber, velocity), 1);
+            }
+
+            eventStart = i; 
+        }
+        
+    }
+    
+    MidiFile.Export("rewritten.mid", collection);
+}
+
+var data = ReadFile();
+WriteFile(data);
+
+var completedString = string.Join('\n', data);
+Console.WriteLine(completedString);
