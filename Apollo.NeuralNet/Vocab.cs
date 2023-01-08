5c7d91964358809be2a7e193e8cf86110d765729
jenboc <47900748+jenboc@users.noreply.github.com>
2023-01-08 15:45:23 +0000

    Cleanup/Reformat

--- Apollo.NeuralNet/Vocab.cs
+++ Apollo.NeuralNet/Vocab.cs
@@ -4,46 +4,43 @@ namespace Apollo.NeuralNet;
 
 public class Vocab
 {
-    private List<char> VocabList { get; }
-
     public Vocab()
     {
-        VocabList = new List<char>(); 
+        VocabList = new List<char>();
     }
 
     public Vocab(List<char> vocabList)
     {
         VocabList = new List<char>();
-        
+
         // Add each character one by one to avoid duplicates 
-        foreach (var c in vocabList)
-        {
-            AddCharacter(c); // So that duplicates are not added 
-        }
+        foreach (var c in vocabList) AddCharacter(c); // So that duplicates are not added 
     }
 
-    public void AddCharacters(char[] characters)
-    {
-        foreach (var c in characters) 
-            AddCharacter(c);
-    }
+    private List<char> VocabList { get; }
 
     /// <summary>
-    /// Retrieve a word from the vocab list using its ID 
+    ///     Retrieve a word from the vocab list using its ID
     /// </summary>
     /// <param name="id">The ID of the character to retrieve</param>
     public char this[int id] => VocabList[id];
-    
+
     /// <summary>
-    /// Retrieve the ID of a character
+    ///     Retrieve the ID of a character
     /// </summary>
     /// <param name="c">The character to retrieve the ID of</param>
     public int this[char c] => VocabList.IndexOf(c);
 
     public int Size => VocabList.Count;
 
+    public void AddCharacters(char[] characters)
+    {
+        foreach (var c in characters)
+            AddCharacter(c);
+    }
+
     /// <summary>
-    /// Add a character to the vocabulary list
+    ///     Add a character to the vocabulary list
     /// </summary>
     /// <param name="c">The character to add to the vocabulary list</param>
     public void AddCharacter(char c)
@@ -52,14 +49,14 @@ public class Vocab
         if (this[c] == -1)
             VocabList.Add(c);
     }
-    
+
     /// <summary>
-    /// Create a one-hot vector for a character in the vocab list
+    ///     Create a one-hot vector for a character in the vocab list
     /// </summary>
     /// <param name="c">The character to represent</param>
     /// <param name="column">Flag variable to decide if the vector is a column vector, true by default</param>
     /// <returns>A column or row one-hot vector</returns>
-    public Matrix CreateOneHot(char c, bool column=true)
+    public Matrix CreateOneHot(char c, bool column = true)
     {
         var vector = new Matrix(Size, 1);
         var charIndex = this[c];
@@ -68,7 +65,7 @@ public class Vocab
             throw new WordNotFoundException("Word does not exist in the vocab list");
 
         vector[charIndex, 0] = 1;
-        
+
         if (!column)
             vector.Transpose();
 
@@ -76,7 +73,7 @@ public class Vocab
     }
 
     /// <summary>
-    /// Interpret a one-hot vector
+    ///     Interpret a one-hot vector
     /// </summary>
     /// <param name="vector">The one-hot vector</param>
     /// <returns>The character represented by the one-hot vector</returns>
@@ -85,25 +82,23 @@ public class Vocab
         // A row or column vector should be passed, meaning the vector should have:
         // 1 row/column
         // vocabSize columns/rows
-        if ((vector.Rows > 1 && vector.Columns > 1) || 
-            (vector.Rows == 1 && vector.Columns != Size) || 
-            (vector.Columns == 1 && vector.Rows != Size)) 
+        if ((vector.Rows > 1 && vector.Columns > 1) ||
+            (vector.Rows == 1 && vector.Columns != Size) ||
+            (vector.Columns == 1 && vector.Rows != Size))
             throw new InvalidOneHotException("A one-hot vector should have shape 1 x vocabSize or vocabSize x 1");
-        
+
         if (vector.Rows == 1 && vector.Columns > 1) // Ensure it is a column vector 
             vector.Transpose();
-        
+
         // Find the index of the 1
         var index = -1;
         var numOnes = 0;
         for (var i = 0; i < vector.Rows; i++)
-        {
             if (vector[i, 0] == 1f)
             {
                 index = i;
                 numOnes++;
             }
-        }
 
         if (numOnes != 1)
             throw new InvalidOneHotException("A one-hot vector should only contain one 1");
@@ -114,10 +109,7 @@ public class Vocab
     public Matrix[] PrepareTrainingData(string midiString)
     {
         var trainingData = new Matrix[midiString.Length];
-        for (var i = 0; i < midiString.Length; i++)
-        {
-            trainingData[i] = CreateOneHot(midiString[i]);
-        }
+        for (var i = 0; i < midiString.Length; i++) trainingData[i] = CreateOneHot(midiString[i]);
 
         return trainingData;
     }
