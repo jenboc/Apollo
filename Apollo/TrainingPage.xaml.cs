6e9633cfdb246867c17269013ab977499810349e
jenboc <47900748+jenboc@users.noreply.github.com>
2023-02-02 18:41:22 +0000

    MinEpochs cannot be greater than max epochs

--- Apollo/TrainingPage.xaml.cs
+++ Apollo/TrainingPage.xaml.cs
@@ -19,6 +19,16 @@ public partial class TrainingPage : Page
     {
         var minEpochs = Convert.ToInt32(MinEpochSlider.Value);
         var maxEpochs = Convert.ToInt32(MaxEpochSlider.Value);
+
+        // minEpochs cannot be bigger than maxEpochs
+        // Avoid training if this is the case
+        if (minEpochs > maxEpochs)
+        {
+            MessageBox.Show("Minimum Epochs cannot be greater than Maximum Epochs", "Invalid Data Entry",
+                MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            return;
+        }
+        
         var maxError = (float)MaxEpochSlider.Value;
         var batchesPerEpoch = Convert.ToInt32(BatchesPerEpochSlider.Value);
 
