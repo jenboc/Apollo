81ed429f0a6b40f254297e37b6358c5f7504ffff
jenboc <47900748+jenboc@users.noreply.github.com>
2023-02-25 17:04:15 +0000

    Change profile

--- Apollo/SettingsPage.xaml.cs
+++ Apollo/SettingsPage.xaml.cs
@@ -1,29 +1,30 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using Apollo.NeuralNet;
 
 namespace Apollo;
 
 public partial class SettingsPage : Page
 {
-    private ProfileManager _profileManager; 
+    private ProfileManager _profileManager;
+    private NeuralNetwork _network; 
     public SettingsPage()
     {
         InitializeComponent();
         _profileManager = (Application.Current as App).ProfileManagement;
+        _network = (Application.Current as App).Network;
         
         AddProfilesToComboBox(); 
     }
 
     private void AddProfilesToComboBox()
     {
-        var network = (Application.Current as App).Network;
-
         for (var i = 0; i < _profileManager.ProfileNames.Length; i++)
         {
             var profileName = _profileManager.ProfileNames[i];
             ProfileComboBox.Items.Add(profileName);
 
-            if (network.CurrentProfile == _profileManager.GetProfile(profileName))
+            if (_network.CurrentProfile == _profileManager.GetProfile(profileName))
                 ProfileComboBox.SelectedIndex = i;
         }
     }
@@ -41,14 +42,21 @@ public partial class SettingsPage : Page
         if (_profileManager.ProfileExists(name))
         {
             MessageBox.Show("A profile already exists with that name");
+            return;
         }
 
         _profileManager.CreateProfile(name);
         ProfileComboBox.Items.Add(name);
+        _network.ChangeProfile(_profileManager.GetProfile(name)); 
     }
 
     private void OnProfileSelected(object sender, RoutedEventArgs e)
     {
+        var selectedIndex = ProfileComboBox.SelectedIndex;
+        var selectedName = (string)ProfileComboBox.Items.GetItemAt(selectedIndex);
+        var selectedProfile = _profileManager.GetProfile(selectedName); 
+        
+        _network.ChangeProfile(selectedProfile);
     }
 
     private string OpenNamePrompt()
