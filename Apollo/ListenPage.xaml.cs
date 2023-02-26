af4254fe9709d8be880f4f320a4d72ce7e1be275
jenboc <47900748+jenboc@users.noreply.github.com>
2023-02-26 16:35:30 +0000

    UI, Stack, Queue

--- Apollo/ListenPage.xaml.cs
+++ Apollo/ListenPage.xaml.cs
@@ -3,70 +3,122 @@ using System.IO;
 using System.Media;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Effects;
 
 namespace Apollo;
 
 public partial class ListenPage : Page
 {
-    private Queue<string> Playlist { get; set; }
-    private SoundPlayer Player { get; set; }
+    private Stack<string> LastPlayed { get; } // Stores songs that have been played
+    private Queue<string> Playlist { get; } // Stores songs that are upcoming 
+    private string? CurrentlyPlaying { get; set; } // Stores path to media which is currently playing 
     
     public ListenPage()
     {
         InitializeComponent();
+        LastPlayed = new Stack<string>();
         Playlist = new Queue<string>();
+        CurrentlyPlaying = null;
+    }
+
+    #region MediaElement Events
+    /// <summary>
+    /// Event which is called when the MediaElement has opened and loaded a piece of media
+    /// </summary>
+    private void OnMediaOpened(object sender, RoutedEventArgs e)
+    {
+        
     }
 
     /// <summary>
-    /// Checks if given path is valid (i.e. references a MIDI file)
+    /// Event which is called when the MediaElement has finished playing a piece of media
     /// </summary>
-    /// <param name="path">The path to check</param>
-    /// <returns>Boolean depicting whether the path is true or not</returns>
-    private bool IsValidPath(string path)
+    private void OnMediaEnded(object sender, RoutedEventArgs e)
     {
-        // The path must end with .mid or .midi
-        // The path must actually exist
-        return (path.EndsWith(".mid") || path.EndsWith(".midi")) && File.Exists(path); 
-    }
-    
-    private void AddButtonPressed(object sender, RoutedEventArgs e)
-    {
-        var path = PathBox.Text;
-
-        if (!IsValidPath(path))
-        {
-            MessageBox.Show("Entered path is not valid");
-            return; 
-        }
+        // Push what was played to the last played stack
+        LastPlayed.Push(CurrentlyPlaying);
         
-        Playlist.Enqueue(path);
+        // Update currently playing from queue
+    }
+    #endregion
+}
 
-        if (Playlist.Count == 1)
-            ChangeSong(); 
+internal class Stack<T>
+{
+    private List<T> _contents;
+    private int _pointer; 
+
+    public Stack()
+    {
+        _contents = new List<T>();
+        _pointer = -1;
     }
 
-    private void ChangeSong()
+    public void Push(T item)
     {
-        string toPlay;
-        var success = Playlist.TryDequeue(out toPlay);
+        _contents.Add(item);
+        _pointer++;
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty())
+            return default;
         
-        // Success is false if the queue is empty 
-        if (!success)
-        {
-            MessageBox.Show("Playlist has finished");
-            return;
-        }
-        
-        // Check that the file still exists before attempting to play it 
-        if (!IsValidPath(toPlay))
-        {
-            MessageBox.Show($"{toPlay} no longer exists");
-            ChangeSong();
-            return; 
-        }
-        
-        // Play the song
-        Player = new SoundPlayer(toPlay);
-        Player.Play();
+        return _contents[_pointer];
+    }
+
+    public T Pop()
+    {
+        if (IsEmpty())
+            return default;
+
+        var popped = Peek(); 
+        _contents.RemoveAt(_pointer);
+        _pointer--;
+        return popped;
+    }
+
+    public bool IsEmpty()
+    {
+        return _contents.Count == 0;
+    }
+}
+
+internal class Queue<T>
+{
+    private List<T> _contents;
+
+    public Queue()
+    {
+        _contents = new List<T>();
+    }
+
+    public void Enqueue(T item)
+    {
+        _contents.Add(item);
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty())
+            return default;
+
+        return _contents[0];
+    }
+
+    public T Dequeue()
+    {
+        if (IsEmpty())
+            return default;
+
+        var dequeued = Peek();
+        _contents.RemoveAt(0);
+        return dequeued;
+    }
+
+    public bool IsEmpty()
+    {
+        return _contents.Count == 0;
     }
 }
